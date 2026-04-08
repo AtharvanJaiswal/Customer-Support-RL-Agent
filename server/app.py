afbfd847c412fd9ef0ebcf4b9e786b2b293a5029
AtharvanJaiswal <atharvanjaiswal@gmail.com>
2026-04-08 19:36:07 +0530

    changes in pyproject

--- server/app.py
+++ server/app.py
@@ -1,3 +1,4 @@
+import uvicorn
 from openenv.core.env_server import create_fastapi_app
 
 from environment import CustomerSupportEnv
@@ -8,3 +9,11 @@ app = create_fastapi_app(
     SupportAction,
     SupportObservation
 )
+
+
+def main():
+    uvicorn.run("server.app:app", host="0.0.0.0", port=8000)
+
+
+if __name__ == "__main__":
+    main()
